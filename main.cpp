4f630591f56e06bcb33754f693f9bb68621cdb35
juan <calvojuancito@gmail.com>
2025-05-17 19:38:15 -0300

    juan-Se creo la clase sandbox para realizar todo tipo de test que querramos

--- main.cpp
+++ main.cpp
@@ -1,47 +1,12 @@
 #include <iostream>
-#include "Persona.h"
 #include <locale>
-#include "ArchivoMedico.h"
-#include "Medico.h"
+#include "Sandbox.h"
 
 using namespace std;
 
 int main() {
 
-    Fecha fechaNac(15, 6, 1985);
-    Fecha fechaIni(1, 3, 2010);
-    Medico medico;
-
-    Medico medicoTest(
-        1,
-        "12345678",
-        "Juan",
-        "Perez",
-        "1123456789",
-        "juan@email.com",
-        fechaNac,
-        3,
-        fechaIni
-    );
-
-    // Crear objeto Archivo
-    ArchivoMedico archivo("medicos.dat");
-
-    // Guardar
-    if (archivo.Guardar(medicoTest)) {
-        std::cout << "Medico guardado correctamente." << std::endl;
-    } else {
-        std::cout << "Error al guardar el médico." << std::endl;
-    }
-
-    int cant = archivo.CantidadRegistros();
-
-    for(int i = 0; i< cant;i++){
-        medico = archivo.Leer(i);
-        cout<<medico.toCSV()<<endl;
-    }
-
-
+    //ejecutarSandboxMedico();
 
     return 0;
 }
